89ca1060b4c045dd6b1b44653d1466069786a1eb
Kevin <kphyde@gmail.com>
2018-11-04 15:45:50 -0800

    add friends.json routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require("path");
 var router = express.Router();
+var friends = require('../data/friends.json')
 
 //Middle ware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -9,10 +10,11 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get("/api/friends", function (req, res) {
+  // res.json(path.join(__dirname, "../data/friends.js"));
   return res.json(friends);
 })
 
-router.post("/api/friends", function (req, res) {
+router.post("/api/friends", function (req, res) { 
   // logid to add a new friend record
   
   res.json(newFriend)
