ceb57b649143c5eaecaaa862d292295bcc4a8926
Kevin <kphyde@gmail.com>
2018-11-04 21:54:06 -0800

    got server.js to read and write to friends.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 var mysql = require("mysql");
+var friends = require("./app/data/friends");
 
 var app = express();
 
@@ -17,8 +19,41 @@ app.use('/static', express.static('data'));
 var PORT = process.env.PORT || 8080;
 
 
+app.post("/survey", function (req, res) {
+  // var data = {
+  //   test: "test",
+  //   test2: "test2"
+  // }
 
+  // fs.appendFile("app/data/friends.json", JSON.stringify(data), function (err) {
+  //   if (err) throw err;
+  //   console.log('complete');
+  // })
+})
 
 app.listen(PORT, function () {
   console.log("listening on port: http://localhost:" + PORT)
 })
+
+
+var myData = {
+  test: "test",
+  test2: "test2"
+}
+
+fs.readFile('app/data/friends.json', 'utf-8', function(err, data) {
+	if (err) throw err
+
+  var arrayOfObjects = JSON.parse(data)
+  arrayOfObjects.push(myData);
+  arrayOfObjects.push(myData);
+  arrayOfObjects.push(myData);
+  console.log(arrayOfObjects);
+})
+
+
+
+// fs.appendFile("app/data/friends.json", JSON.stringify(data), function (err) {
+//   if (err) throw err;
+//   console.log('complete');
+// })
