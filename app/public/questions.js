1d27f78814e31e250090d60ac4647368fd1ef686
Kevin <kphyde@gmail.com>
2018-11-04 21:18:16 -0800

    working to get static .js file to load in express . . . write stackoverflow to find out

--- app/public/questions.js
+++ app/public/questions.js
@@ -1,3 +1,5 @@
+alert("js loaded");
+
 var questions = {
   1 : "I see myself as extraverted, enthusiastic.",
   2 : "I see myself as critical, quarrelsome.",
@@ -7,3 +9,10 @@ var questions = {
   6 : "I see myself as reserved, quiet.",
   7 : "I see myself as sympathetic, warm."
 }
+
+var dom_question = document.getElementById('question_label');
+var answer_selections = document.getElementsByName('radio');
+
+$("#submit").on("click", function () {
+  console.log(dom_question + answer_selections);
+});
--- server.js
+++ server.js
@@ -7,9 +7,12 @@ var app = express();
 
 app.use(require('./app/routing/apiRoutes'))
 app.use(require('./app/routing/htmlRoutes'))
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static('data'));
+app.use('/static', express.static('public'));
+app.use('/static', express.static('data'));
 
 var PORT = process.env.PORT || 8080;
 
